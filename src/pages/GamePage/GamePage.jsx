fa296374c7d4032901f1a3a09f3fde3a810eb961
«Annavtropikah» <kozhevnikova.it@gmail.com>
2024-06-21 20:20:48 +0300

    курмовая

--- src/pages/GamePage/GamePage.jsx
+++ src/pages/GamePage/GamePage.jsx
@@ -4,11 +4,6 @@ import { Cards } from "../../components/Cards/Cards";
 
 export function GamePage() {
   const { pairsCount } = useParams();
-  const { isGameMode } = useParams();
-
-  return (
-    <>
-      <Cards pairsCount={parseInt(pairsCount, 10)} previewSeconds={5} isGameMode={isGameMode}></Cards>
-    </>
-  );
+  const { isEasyMode } = useParams();
+  return <Cards pairsCount={parseInt(pairsCount, 10)} previewSeconds={5} isEasyMode={isEasyMode} />;
 }
